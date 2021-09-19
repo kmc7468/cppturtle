5c272c5f352286e6f0f2821d1cc8659c5c2584d9
static <kmc7468@naver.com>
2021-09-19 20:48:41 +0900

    Window 클래스의 이름을 MainWindow로 변경 및 내부 구조 변경

--- cppturtle.hpp
+++ cppturtle.hpp
@@ -30,15 +30,14 @@
 #	error "C++11이 지원되는 환경에서만 사용할 수 있습니다."
 #else
 
+#include <algorithm>
 #include <cmath>
 #include <stdexcept>
-#include <string>
 #include <utility>
+#include <vector>
 
 #include <Windows.h>
 #include <tchar.h>
-
-#define CT_WINAPI_SAFELY(expression) ((SetLastError(0), (expression), GetLastError()) == 0)
 
 namespace turtle {
 	class Vector2 {
@@ -63,7 +62,8 @@ namespace turtle {
 
 	public:
 		float GetSize() const {
-			return std::sqrtf(X * X + Y * Y);
+			return std::sqrtf(static_cast<float>(X) * static_cast<float>(X) +
+				static_cast<float>(Y) * static_cast<float>(Y));
 		}
 		int Dot(const Vector2& rhs) const noexcept {
 			return X * rhs.X + Y * rhs.Y;
@@ -92,7 +92,7 @@ namespace turtle {
 		Vector2f(float x, float y) noexcept
 			: X(x), Y(y) {}
 		Vector2f(const Vector2& other) noexcept
-			: X(other.X), Y(other.Y) {}
+			: X(static_cast<float>(other.X)), Y(static_cast<float>(other.Y)) {}
 		Vector2f(const Vector2f& other) noexcept = default;
 		~Vector2f() = default;
 
@@ -147,101 +147,176 @@ namespace turtle {
 	}
 }
 
+#define CT_WINAPI_SAFELY(expression) ((SetLastError(0), (expression), GetLastError()) == 0)
+
+#define CT_WM_DESTROY WM_APP + 0
+
 namespace turtle {
-	class Window {
-	private:
-		HWND m_Handle = nullptr;
+	namespace detail {
+		class Window {
+		private:
+			HWND m_Handle = nullptr;
+			HMENU m_Menu = nullptr;
+			WNDPROC m_WndProc = nullptr;
+			std::vector<HWND> m_Children;
+
+		public:
+			Window(HWND parent, LPCTSTR className, LPCTSTR windowName, DWORD style, DWORD exStyle, Vector2 location, Vector2 size) {
+				Window* const parentWrapper = GetWrapper(parent);
+				if (parentWrapper) {
+					m_Menu = reinterpret_cast<HMENU>(parentWrapper->m_Children.size());
+				}
+
+				m_Handle = CreateWindowEx(exStyle, className, windowName, style,
+					location.X, location.Y, size.X, size.Y, parent, m_Menu, GetModule(), nullptr);
+				if (!m_Handle) throw std::runtime_error("윈도우를 생성하지 못했습니다.");
+
+				if (!CT_WINAPI_SAFELY(SetWindowLongPtr(m_Handle, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this)))) {
+					DestroyWindow(m_Handle);
+					throw std::runtime_error("윈도우를 초기화하지 못했습니다.");
+				}
+
+				m_WndProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(m_Handle, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WndProcStatic)));
+				if (!m_WndProc) {
+					DestroyWindow(m_Handle);
+					throw std::runtime_error("윈도우를 초기화하지 못했습니다.");
+				}
+
+				if (parentWrapper) {
+					try {
+						parentWrapper->m_Children.push_back(m_Handle);
+					} catch (...) {
+						DestroyWindow(m_Handle);
+						throw std::runtime_error("윈도우를 초기화하지 못했습니다.");
+					}
+				}
+			}
+			Window(Window&& other) {
+				Move(std::move(other));
+			}
+			virtual ~Window() {
+				Destroy();
+			}
+
+		public:
+			Window& operator=(Window&& other) {
+				Move(std::move(other));
+				return *this;
+			}
+
+		public:
+			void Destroy() noexcept {
+				SendMessage(m_Handle, CT_WM_DESTROY, 0, 0);
+
+				Window* const parent = GetWrapper(GetParent(m_Handle));
+				if (parent) {
+					*std::find(parent->m_Children.begin(), parent->m_Children.end(), m_Handle) = nullptr;
+				}
+
+				for (auto child : m_Children) {
+					if (!child) continue;
+
+					GetWrapper(child)->Destroy();
+				}
+
+				SetWindowLongPtr(m_Handle, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(m_WndProc));
+
+				DestroyWindow(m_Handle);
+
+				m_Handle = nullptr;
+				m_Menu = nullptr;
+				m_WndProc = nullptr;
+				m_Children.clear();
+			}
+
+		protected:
+			static HINSTANCE GetModule() noexcept {
+				static HINSTANCE module = GetModuleHandle(nullptr);
+				return module;
+			}
+
+			HWND GetHandle() const noexcept {
+				return m_Handle;
+			}
+			static Window* GetWrapper(HWND handle) noexcept {
+				return reinterpret_cast<Window*>(GetWindowLongPtr(handle, GWLP_USERDATA));
+			}
+
+			static LRESULT CALLBACK WndProcStatic(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
+				return reinterpret_cast<Window*>(GetWindowLongPtr(handle, GWLP_USERDATA))->WndProc(handle, message, wParam, lParam);
+			}
+			virtual LRESULT WndProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
+				return CallWindowProc(m_WndProc, m_Handle, message, wParam, lParam);
+			}
+
+		private:
+			void Move(Window&& other) {
+				if (CT_WINAPI_SAFELY(SetWindowLongPtr(other.m_Handle, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this)))) {
+					Destroy();
+
+					std::swap(m_Handle, other.m_Handle);
+					std::swap(m_Menu, other.m_Menu);
+					std::swap(m_WndProc, other.m_WndProc);
+					std::swap(m_Children, other.m_Children);
+				} else throw std::runtime_error("윈도우를 이동하지 못했습니다.");
+			}
+		};
+
+		struct UserWindowClass {
+			UserWindowClass(void generator()) {
+				static bool dummy = (generator(), true);
+			}
+		};
+	}
+
+	class MainWindow : public detail::UserWindowClass, public detail::Window {
+	public:
+		MainWindow()
+			: detail::UserWindowClass(CreateWindowClass),
+			detail::Window(nullptr, _T("ctWindow"), _T("cppturtle"), WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0,
+				{ CW_USEDEFAULT, CW_USEDEFAULT }, { 640, 480 }) {
+			++GetWindowCount();
+		}
+		MainWindow(MainWindow&& other) = default;
+		virtual ~MainWindow() override = default;
 
 	public:
-		Window() {
-			CreateHandle();
-		}
-		Window(Window&& other) {
-			MoveHandle(std::move(other));
-		}
-		~Window() {
-			DestroyWindow(m_Handle);
-		}
+		MainWindow& operator=(MainWindow&& other) = default;
 
-	public:
-		Window& operator=(Window&& other) {
-			MoveHandle(std::move(other));
-			return *this;
+	protected:
+		virtual LRESULT WndProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) override {
+			switch (message) {
+			case WM_CLOSE:
+				Destroy();
+				return 0;
+
+			case CT_WM_DESTROY:
+				if (--GetWindowCount() == 0) {
+					PostQuitMessage(0);
+				}
+				return 0;
+
+			default:
+				return detail::Window::WndProc(handle, message, wParam, lParam);
+			}
 		}
 
 	private:
-		static HINSTANCE GetModule() noexcept {
-			static HINSTANCE module = GetModuleHandle(nullptr);
-			return module;
-		}
-		static LPCTSTR CreateWindowClass() {
+		static void CreateWindowClass() {
 			WNDCLASS wndClass{};
 			wndClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
 			wndClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
 			wndClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
 			wndClass.hInstance = GetModule();
-			wndClass.lpfnWndProc = WndProcStatic;
+			wndClass.lpfnWndProc = DefWindowProc;
 			wndClass.lpszClassName = _T("ctWindow");
 			wndClass.style = CS_HREDRAW | CS_VREDRAW;
 
-			if (RegisterClass(&wndClass)) return wndClass.lpszClassName;
-			else throw std::runtime_error("윈도우 클래스를 생성하지 못했습니다.");
+			if (!RegisterClass(&wndClass)) throw std::runtime_error("윈도우 클래스를 생성하지 못했습니다.");
 		}
-		void CreateHandle() {
-			static LPCTSTR className = CreateWindowClass();
-
-			if (!CreateWindowEx(0, className, _T("cppturtle"), WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-				CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, nullptr, nullptr, GetModule(), this))
-				throw std::runtime_error("윈도우를 생성하지 못했습니다.");
-		}
-		void MoveHandle(Window&& other) {
-			if (CT_WINAPI_SAFELY(SetWindowLongPtr(other.m_Handle, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this)))) {
-				DestroyWindow(m_Handle);
-
-				m_Handle = other.m_Handle;
-				other.m_Handle = nullptr;
-			} else throw std::runtime_error("윈도우를 이동하지 못했습니다.");
-		}
-
-		static LRESULT CALLBACK WndProcStatic(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
-			Window* window = reinterpret_cast<Window*>(GetWindowLongPtr(handle, GWLP_USERDATA));
-			if (!window && message == WM_CREATE) {
-				CREATESTRUCT* const createStruct = reinterpret_cast<CREATESTRUCT*>(lParam);
-
-				window = reinterpret_cast<Window*>(createStruct->lpCreateParams);
-				window->m_Handle = handle;
-
-				if (!CT_WINAPI_SAFELY(SetWindowLongPtr(handle, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window)))) {
-					DestroyWindow(handle);
-					throw std::runtime_error("윈도우를 초기화하지 못했습니다.");
-				}
-			}
-
-			return window ? window->WndProc(message, wParam, lParam) : DefWindowProc(handle, message, wParam, lParam);
-		}
-		LRESULT WndProc(UINT message, WPARAM wParam, LPARAM lParam) {
-			static unsigned windowCount = 0;
-
-			switch (message) {
-			case WM_CREATE:
-				++windowCount;
-				return 0;
-
-			case WM_CLOSE:
-				DestroyWindow(m_Handle);
-				return 0;
-
-			case WM_DESTROY:
-				if (GetWindowLongPtr(m_Handle, GWLP_USERDATA) && --windowCount == 0) {
-					PostQuitMessage(0);
-				}
-
-				m_Handle = nullptr;
-				return 0;
-
-			default:
-				return DefWindowProc(m_Handle, message, wParam, lParam);
-			}
+		static unsigned& GetWindowCount() noexcept {
+			static unsigned count = 0;
+			return count;
 		}
 
 	public:
@@ -257,7 +332,7 @@ namespace turtle {
 				DispatchMessage(&message);
 			}
 
-			return message.wParam;
+			return static_cast<int>(message.wParam);
 		}
 	};
 }
